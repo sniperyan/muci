bf53c83cbf58c95b17d6d9e1abacb93125ed24c2
sniperyan <304787171@qq.com>
2018-05-28 15:52:54 +0800

    vendor bugfix

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -104,7 +104,8 @@ const webpackConfig = merge(baseWebpackConfig, {
           module.resource &&
           /\.js$/.test(module.resource) &&
           module.resource.indexOf(
-            path.join(__dirname, '../node_modules')
+            // path.join(__dirname, '../node_modules')
+            utils.resolve('node_modules')
           ) === 0
         )
       }
