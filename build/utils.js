e6e5f07bdeba5692e04895e712e75f90d88b6e3f
sniperyan <304787171@qq.com>
2018-06-14 17:53:10 +0800

    css modules

--- build/utils.js
+++ build/utils.js
@@ -20,6 +20,58 @@ exports.cssLoaders = function(options) {
     loader: 'css-loader',
     options: {
       sourceMap: options.sourceMap,
+    },
+  };
+
+  const postcssLoader = {
+    loader: 'postcss-loader',
+    options: {
+      sourceMap: options.sourceMap,
+    },
+  };
+
+  // generate loader string to be used with extract text plugin
+  function generateLoaders(loader, loaderOptions) {
+    const loaders = options.usePostCSS ? [cssLoader, postcssLoader] : [cssLoader];
+
+    if (loader) {
+      loaders.push({
+        loader: loader + '-loader',
+        options: Object.assign({}, loaderOptions, {
+          sourceMap: options.sourceMap,
+        }),
+      });
+    }
+
+    // Extract CSS when that option is specified
+    // (which is the case during production build)
+    if (options.extract) {
+      return ExtractTextPlugin.extract({
+        use: loaders,
+      });
+    } else {
+      return ['style-loader'].concat(loaders);
+    }
+  }
+
+  return {
+    css: generateLoaders(),
+    postcss: generateLoaders(),
+    less: generateLoaders('less'),
+    sass: generateLoaders('sass', { indentedSyntax: true }),
+    scss: generateLoaders('sass'),
+    stylus: generateLoaders('stylus'),
+    styl: generateLoaders('stylus'),
+  };
+};
+
+exports.cssModuleLoaders = function(options) {
+  options = options || {};
+
+  const cssLoader = {
+    loader: 'css-loader',
+    options: {
+      sourceMap: options.sourceMap,
       modules: true,
       localIdentName: '[path][name]__[local]--[hash:base64:5]'
     },
@@ -70,13 +122,23 @@ exports.cssLoaders = function(options) {
 // Generate loaders for standalone style files
 exports.styleLoaders = function(options) {
   const output = [];
-  const loaders = exports.cssLoaders(options);
+  const modulesLoaders = exports.cssModuleLoaders(options);
+  const commonLoaders = exports.cssLoaders(options);
 
-  for (const extension in loaders) {
-    const loader = loaders[extension];
+  for (const extension in modulesLoaders) {
+    const loader = modulesLoaders[extension];
     output.push({
       test: new RegExp('\\.' + extension + '$'),
       use: loader,
+      exclude: /node_modules/,
+    });
+  }
+  for (const extension in commonLoaders) {
+    const loader = commonLoaders[extension];
+    output.push({
+      test: new RegExp('\\.' + extension + '$'),
+      use: loader,
+      include: /node_modules/,
     });
   }
 
